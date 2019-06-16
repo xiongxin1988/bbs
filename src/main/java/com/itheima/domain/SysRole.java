d357502f59892b9faf375c7f49fdc5b18513148e
xiongxin1988 <361292307@qq.com>
2019-06-16 21:27:21 +0800

    完善权限类

--- src/main/java/com/itheima/domain/SysRole.java
+++ src/main/java/com/itheima/domain/SysRole.java
@@ -1,4 +1,23 @@
 package com.itheima.domain;
 
 public class SysRole {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
